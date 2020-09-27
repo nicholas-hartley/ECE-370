4989da6dfb02c4b48ea4fb0433a16c4be86f1869
None yobidness <nhartley@crimson.ua.edu>
2020-09-27 17:38:11 -0500

    I think part 4 is done

--- Project 2/Untitled2.m
+++ Project 2/Untitled2.m
@@ -1,3 +1,4 @@
+
 clear all;
 close all;
 
@@ -8,33 +9,38 @@ n = linspace(0,N, 4*N); % I like the smoother curves
 
 load('lineup.mat');
 
-he = [1 zeros(1,998) a]; % Model dirac delta using comparison
+% This is part a
+he = [1 zeros(1,998) a]; % There should only be an impulse at n = 0, 1000
+
+
+% This is part c
+
+a1 = [1 zeros(1,998) a]; % System coefficients for z
+b1 = 1;
 
 d = [1 zeros(1,4000)];
 
-a1 = [1 a];
-
-b1 = 1;
-
-her = filter(b1, a1, d);
+her = filter(1, a1, d);
 
 z = filter(b1, a1, y);
-plot(y);
+plot(y, 'b', 'DisplayName', 'y');
 
 hold on;
 
-plot(z, 'r');
+plot(z, 'r', 'DisplayName', 'z');
 
-% sound(y, 8192);
-% 
-% pause(3);
-% 
-% sound(z, 8192);
+legend('boxon');
 
-hoa = conv(her, he);
+sound(y, 8192);
+
+pause(3);
+
+sound(z, 8192);
+
+hoa = conv(he, her);
 
 figure(2);
 plot(he, '--g');
 hold on;
 plot(her, '.r');
-plot(hoa);
+plot(hoa, '-b', 'LineWidth', 2);
